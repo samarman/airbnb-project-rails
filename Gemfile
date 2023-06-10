7ca278919db2b7139a11c3773c5c5d0b16abe6c0
sam arman <97samarman@gmail.com>
2023-06-10 16:31:55 +0100

    added the search function to index

--- Gemfile
+++ Gemfile
@@ -79,3 +79,4 @@ end
 gem 'devise'
 gem 'pundit'
 gem "cloudinary"
+gem "pg_search"
--- app/models/planet.rb
+++ app/models/planet.rb
@@ -4,4 +4,10 @@ class Planet < ApplicationRecord
   validates :name, :description, :price_per_night, presence: true
   validates :name, uniqueness: true
   has_one_attached :photo
+
+  include PgSearch::Model
+  pg_search_scope :search_by_name_address_and_description,
+  against: [:planet, :address, :description], using: {
+    tsearch: { prefix: true }
+  }
 end
