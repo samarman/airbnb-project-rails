4ad11938005b0275e5a0f2967ae8c551e358ff88
tariq khan <tariqjameskhan@hotmail.co.uk>
2023-06-13 13:34:53 +0100

    create review function works, created pundit policy

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,3 +1,20 @@
 class ReviewsController < ApplicationController
+  def create
+    @planet = Planet.find(params[:planet_id])
+    @review = Review.new(review_params)
+    @review.planet = @planet
+    @review.user = current_user
+    authorize @review
+    if @review.save
+      redirect_to planet_path(@planet)
+    else
+      render planet_path(@planet), status: :unprocessable_entity
+    end
+  end
 
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :content)
+  end
 end
