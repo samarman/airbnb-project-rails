2f6e11ed504609e13499811828fac2f6f0119820
tariq khan <tariqjameskhan@hotmail.co.uk>
2023-06-13 12:29:08 +0100

    added the review model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_04_201920) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_13_112722) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -65,6 +65,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_04_201920) do
     t.index ["user_id"], name: "index_planets_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "content"
+    t.bigint "planet_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["planet_id"], name: "index_reviews_on_planet_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -84,4 +95,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_04_201920) do
   add_foreign_key "bookings", "planets"
   add_foreign_key "bookings", "users"
   add_foreign_key "planets", "users"
+  add_foreign_key "reviews", "planets"
+  add_foreign_key "reviews", "users"
 end
