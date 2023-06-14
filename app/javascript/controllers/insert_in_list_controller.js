5fc7e04b87be530bbb59286ee122c934fc563f20
tariq khan <tariqjameskhan@hotmail.co.uk>
2023-06-14 13:02:28 +0100

    working AJAX reviews

--- app/javascript/controllers/insert_in_list_controller.js
+++ app/javascript/controllers/insert_in_list_controller.js
@@ -8,11 +8,26 @@ export default class extends Controller {
     // console.log("my form", this.formTarget)
     // console.log("my items", this.itemsTarget)
     // console.log("my controller", this.element)
+    this.csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute("content")
   }
 
   send(event) {
     event.preventDefault()
-    console.log(event)
+    // console.log(event)
+    const url = this.formTarget.action
+    const options = {
+      method: "POST",
+      headers: { "Accept": "application/json", "X-CSRF-Token": this.csrfToken },
+      body: new FormData(this.formTarget)
+    }
     fetch(url, options)
+    .then(response => response.json())
+    .then((data) => {
+      // console.log(data)
+      // console.log('review', data.inserted_item)
+      // console.log('form', data.form)
+      this.itemsTarget.insertAdjacentHTML('afterbegin', data.inserted_item)
+
+    })
   }
 }
